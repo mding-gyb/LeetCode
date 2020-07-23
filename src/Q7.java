23f7de60d58d97dde3d6bbc8d5a0bc3881391ce7
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-07-23 00:09:24 -0400

    08/23

---
+++ src/Q7.java
@@ -0,0 +1,11 @@
+
+public class Q7 {
+	public int reverseInt(int x) {
+        long res = 0;
+        while(x != 0) {
+        	res = res * 10 + x % 10;
+        	x = x/10;
+        }
+        return res < Integer.MIN_VALUE || res > Integer.MAX_VALUE ? 0 : (int)res;
+    }
+}

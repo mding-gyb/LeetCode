f6002a72b6a505670d013dedfbb7fe3df94ed0b9
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-07-26 00:27:19 -0400

    07/26

--- src/Q8.java
+++ src/Q8.java
@@ -1,32 +1,60 @@
 import java.util.ArrayList;
 public class Q8 {
 	public static void main(String[] args) {
-		System.out.println(myAtoi("  15"));
+		System.out.println(myAtoi("+-2"));
 	}
 	public static int myAtoi(String str) {
 		String out = "";
 		int index = 0;
 		ArrayList <String> checks = new ArrayList<String>();
 		
-		for(int i = 1; i < 10; i++) {
+		for(int i = 0; i < 10; i++) {
 			checks.add(Integer.toString(i));
 		}
-		
-		checks.add("-");
-		
-		while (str.charAt(index) == ' ') {
-			System.out.println(index);
-			index ++;
+		try {
+			while (str.charAt(index) == ' ') {
+				index ++;
+			}
+			if (str.charAt(index) == '+') {
+				out += "+";
+                index++;
+			} else if (str.charAt(index) == '-') {
+				out += "-";
+                index++;
+			}
+			
+			if(!checks.contains(Character.toString(str.charAt(index)))) {
+				return 0;
+			} 
+		} catch (Exception e) {
+			
 		}
-		if(!checks.contains(Character.toString(str.charAt(index)))) {
-			return 0;
-		} 
 		
-		while (checks.contains(Character.toString(str.charAt(index))) && index < str.length()) {
-			System.out.println(index);
-			out += Character.toString(str.charAt(index));
-			index++;	
+		try {
+			while (checks.contains(Character.toString(str.charAt(index)))) {
+				out += Character.toString(str.charAt(index));
+				index++;	
+			}
+		} catch (Exception e) {
+			
 		}
-		return Integer.parseInt(out);
+		try {
+			
+			return out.equals("") ? 0 : Integer.parseInt(out);
+		} catch (Exception e) {
+			if (out.charAt(0) == '-') {
+				if(out.length() != 1) {
+					return Integer.MIN_VALUE;
+				}
+				return 0;
+			} else if (out.charAt(0) == '+') {
+                if(out.length() != 1) {
+					return Integer.MAX_VALUE;
+				}
+				return 0;
+			} else {
+                return Integer.MAX_VALUE;
+            }
+		}	
 	}
 }

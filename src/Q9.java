f296c51557190b4b48ae7bbe2775aaba248ecf45
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-07-27 17:57:33 -0400

    07/27

---
+++ src/Q9.java
@@ -0,0 +1,11 @@
+
+public class Q9 {
+	public static void main(String[]args) {
+		System.out.println(isPalindrome(-121));
+	}
+	public static boolean isPalindrome(int x) {
+		
+        StringBuilder input1 = new StringBuilder();
+        return Integer.toString(x).equals(input1.append(Integer.toString(x)).reverse().toString()) ? true : false;
+    }
+}

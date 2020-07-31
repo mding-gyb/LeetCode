1ecdcc3c5791e82e5dbb6046c3c05a3069c43bbb
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-07-30 21:19:03 -0400

    07/30

--- src/Q10.java
+++ src/Q10.java
@@ -1,7 +1,37 @@
 
 public class Q10 {
 	public boolean isMatch(String s, String p) {
-		
+		if (s.length() != p.length()) {
+			return false;
+		}
+		if (s.length() == 0) {
+			return true;
+		}
+		int count = 1;
+		char s_previous = s.charAt(0);
+		char p_previous = p.charAt(0);
+		while(count < s.length()) {
+			char s_char = s.charAt(count);
+			char p_char = p.charAt(count);
+			if(s.charAt(count) == '*') {
+				s_char = s_previous;
+			}
+			if(p.charAt(count) == '*') {
+				p_char = p_previous;
+			}
+			if(s_char != p_char) {
+				if(s_char != '.' || p_char != '.') {
+					return false;
+				} else if (s_char == '.' && p_char == '.') {
+					
+				} else {
+					s_char = s_char == '.' ? p_char : s_char;
+					p_char = p_char == '.' ? s_char : p_char;
+				}
+			}
+			s_previous = s_char;
+			p_previous = p_char;
+		}
         return true;
     }	
 }

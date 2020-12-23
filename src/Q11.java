998d977bd8c3b3e22c1b707d6e1a6f0f60423780
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-12-23 13:44:42 -0500

    stego

--- src/Q11.java
+++ src/Q11.java
@@ -1,4 +1,23 @@
-
+import java.math.*;
 public class Q11 {
-
+	public int maxArea(int[] height) {
+		if (height.length == 2) {
+			return Math.min(height[0], height[1]);
+		} else {
+			return calcMax(height);
+		}
+	}
+	private int calcMax(int[]a) {
+		int max = 0;
+		for (int i = 0; i < a.length; i++) { 
+			for (int j = i; j < a.length; j++) {
+				int temp = (j - i) * Math.min(a[i], a[j]);
+				if (temp > max) {
+					max = temp;
+				}
+			}
+		}
+		return max;
+		
+	}
 }
